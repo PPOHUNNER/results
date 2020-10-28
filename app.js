0a3ae46690edb40433689d291348f3dd70fd21bd
ppohunner <prajwalshiv.04@gmail.com>
2020-10-28 08:38:33 +0530

    added home page

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ mongoose.connect(uri, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 
-
+app.get("/",(req,res)=>{
+    res.render("home");
+})
 app.get("/results",(req,res)=>{
     res.render("result_form",{data:null});
 });
